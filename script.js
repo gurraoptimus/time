7a361deb3419e3db99f200d84c69d0a38f378bea
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-03 14:42:45 +0200

    script

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ const numberElemnt=[];
 
 for (let i= 1; i <=12; i++){
     console.log(numberElemnt);
-    numberElemnt.push("<span><p>${i}</p></span>");
+    numberElemnt.push(<span><p>${i}</p></span>);
 }
