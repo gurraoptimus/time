951b7dc27a163ee1f3f9600d6219da61cf1d20b7
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-03 14:58:11 +0200

    script

--- script.js
+++ script.js
@@ -10,4 +10,3 @@ for (let i= 1; i <=12; i++){
     numberElemnt.push(<span style="--index:${i};"><p>${i}</p></span>);
 }
 numberHours.insertAdjacentHTML("afterbegin", numberElemnt.join(""));
-console.log(numberElemnt);
