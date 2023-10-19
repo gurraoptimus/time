3af2ec423246b60261a363185161c19e3c3d3fc1
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-19 15:34:00 +0200

    src

--- script.js
+++ script.js
@@ -32,5 +32,5 @@ function getCurrentTime(){
     let CurrentMinutes = date.getMinutes();
     let CurrentSeconds = date.getSeconds();
     
-    handSeconds.Style.transform='rotate()';
+    handSeconds.Style.transform='rotate(${cu)';
 }
