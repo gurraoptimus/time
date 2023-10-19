80aa35496a7bcb6f0cf6c2d2549941c5bf1b3d7e
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-19 15:44:08 +0200

    src

--- script.js
+++ script.js
@@ -32,5 +32,9 @@ function getCurrentTime(){
     let CurrentMinutes = date.getMinutes();
     let CurrentSeconds = date.getSeconds();
     
-    handSeconds.Style.transform=`rotate(${currentSeconds* 6}deg)`;
+    handSeconds.Style.transform=`rotate(${currentSeconds * 6}deg)`;
 }
+
+///////////////////////////////////////////////
+//call getCurrentTime function on page load///
+/////////////////////////////////////////////
